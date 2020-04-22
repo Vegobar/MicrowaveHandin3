8247fcee0b25804ae3b8e1f7ef2f00d2190b17d4
Mikkel Schmidt Andersen <mikkel220897@hotmail.com>
2020-04-22 11:52:09 +0200

    We started on doing the Door integration test as a group. All group members was part of this

---
+++ MicrowaveOven-master/Microwave.Test.Integration/IT_1A_Door.cs
@@ -0,0 +1,42 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT_1A_Door
+    {
+        private UserInterface _userInterface;
+        private Door _uut_door;
+
+        private ILight _light;
+        private IDisplay _display;
+        private IButton _start, _power, _time;
+        private ICookController _cooker;
+
+        [SetUp]
+        public void Setup()
+        {
+            _light = Substitute.For<ILight>();
+            _start = Substitute.For<IButton>();
+            _power = Substitute.For<IButton>();
+            _time = Substitute.For<IButton>();
+            _display = Substitute.For<IDisplay>();
+            _cooker = Substitute.For<ICookController>();
+
+            _uut_door = new Door();
+            _userInterface = new UserInterface(_power, _time, _start, _uut_door, _display, _light, _cooker);
+        }
+
+        [Test]
+        public void blabla()
+        {
+        }
+    }
+}
