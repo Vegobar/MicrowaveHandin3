327e808c515a4bc6e96878682b1774e9acd9ad96
Lena Todnem <lena.todnem@gmail.com>
2020-04-22 14:42:36 +0200

    New test added

--- MicrowaveOven-master/Microwave.Test.Integration/IT_1A_Door.cs
+++ MicrowaveOven-master/Microwave.Test.Integration/IT_1A_Door.cs
@@ -30,7 +30,6 @@ namespace Microwave.Test.Integration
             _time = Substitute.For<IButton>();
             _display = Substitute.For<IDisplay>();
             _cooker = Substitute.For<ICookController>();
-            _output = Substitute.For<IOutput>();
 
             _uut_door = new Door();
             _userInterface = new UserInterface(_power, _time, _start, _uut_door, _display, _light, _cooker);
@@ -42,10 +41,14 @@ namespace Microwave.Test.Integration
             _uut_door.Open();
             _uut_door.Close();
 
-            _light.Received(1).TurnOff();
+            _light.Received(1).TurnOff();               
+        }
 
-            //_output.Received(1).OutputLine("Light is turned off");
-               
+        [Test]
+        public void OpenDoor()
+        {
+            _uut_door.Open();
+            _light.Received(1).TurnOn();
         }
     }
 }
