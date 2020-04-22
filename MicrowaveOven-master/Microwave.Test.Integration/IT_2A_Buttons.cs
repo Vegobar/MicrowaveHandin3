36e2129be9457ac0b9cadb058020fed19c48f22d
Jonas-au18 <Jonas.sdu@gmail.com>
2020-04-22 14:33:59 +0200

    Setup for step 2.

---
+++ MicrowaveOven-master/Microwave.Test.Integration/IT_2A_Buttons.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Boundary;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class IT_2A_Buttons
+    {
+        private UserInterface _uut;
+        private Button _powerButton;
+        private Button _timeButton;
+        private Button _startCancelButton;
+        private Door _door;
+        private IDisplay _display;
+        private ILight _light;
+        private ICookController _cookController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            _display = Substitute.For<IDisplay>();
+            _light = Substitute.For<ILight>();
+            _cookController = Substitute.For<ICookController>();
+            _uut = new UserInterface(
+                _powerButton,
+                _timeButton,
+                _startCancelButton,
+                _door,
+                _display,
+                _light,
+                _cookController);
+
+        }
+    }
+}
