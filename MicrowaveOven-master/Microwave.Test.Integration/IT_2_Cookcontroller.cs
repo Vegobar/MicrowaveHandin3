219cc46488cbf8f0183ddbaa23abfc995764d1b3
Mikkel Schmidt Andersen <mikkel220897@hotmail.com>
2020-04-22 15:57:59 +0200

    Added IT 2 - Cookcontroller

---
+++ MicrowaveOven-master/Microwave.Test.Integration/IT_2_Cookcontroller.cs
@@ -0,0 +1,67 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT_2_Cookcontroller
+    {
+        private UserInterface _userInterface;
+        private Button _powerButton;
+        private Button _timeButton;
+        private Button _startCancelButton;
+        private Door _door;
+        private CookController _cookController;
+
+        private IDisplay _display;
+        private ILight _light;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+
+
+            _display = Substitute.For<IDisplay>();
+            _light = Substitute.For<ILight>();
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+
+            _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
+
+            _userInterface = new UserInterface(
+                _powerButton,
+                _timeButton,
+                _startCancelButton,
+                _door,
+                _display,
+                _light,
+                _cookController);
+        }
+
+        [TestCase]
+        public void StartCooking_called()
+        {
+            //act
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+
+            //Assert
+            _powerTube.Received(1).TurnOn(50);
+        }
+
+    }
+}
